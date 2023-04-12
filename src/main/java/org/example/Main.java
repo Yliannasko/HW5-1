9edd2e630932fb3152f4e03ee4915fe58010f2f5
Yulia <ylianna_sko@mail.ru>
2023-04-12 12:50:59 +0300

    two

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,25 @@
-package org.example;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BonusService service = new BonusService();
+        long bonus = service.calculate(1_000, true);
+        System.out.println(bonus);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        long bonus2 = service.calculate(1_000_000, true);
+        System.out.println(bonus2);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        long bonus3 = service.calculate(16_666, true);
+        System.out.println(bonus3);
+
+        long bonus4 = service.calculate(16_667, true);
+        System.out.println(bonus4);
+
+        long bonus5 = service.calculate(16_668, true);
+        System.out.println(bonus5);
+
+        long bonus6 = service.calculate(50_000, false);
+        System.out.println(bonus6);
+
+        long bonus7 = service.calculate(50_001, false);
+        System.out.println(bonus7);
     }
 }
